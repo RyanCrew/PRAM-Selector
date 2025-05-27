017a2cca7066e57b329b82c37dc8248583df2ded
Artulodaa <83349874+Artulodaa@users.noreply.github.com>
2025-05-27 23:44:32 +0200

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,18 @@
-self.addEventListener('install', event
-=> {
-event.waitUntil(
-caches.open('pram-selector-
-v1').then(cache => {
-return cache.addAll([
-'./Pramv1.html',
-'./icon.png'
-]);
-})
-);
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('pram-selector-v1').then(cache => {
+            return cache.addAll([
+                './index.html',
+                './icon.png'
+            ]);
+        })
+    );
 });
-self.addEventListener('fetch', event =>
-{
-event.respondWith(
-caches.match(event.request).then(res
-ponse => {
-return response ||
-fetch(event.request);
-})
-);
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
