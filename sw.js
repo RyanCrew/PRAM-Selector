e146eb4d03bc59b14768e1ee3f548fa1c0219c13
APALUI <83349874+RyanCrew@users.noreply.github.com>
2025-05-28 17:41:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,6 +11,16 @@ self.addEventListener('install', event => {
     );
 });
 
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(name => name !== 'pram-selector-v1').map(name => caches.delete(name))
+            );
+        })
+    );
+});
+
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
