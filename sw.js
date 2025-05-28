5f3b6f5a1e12d91badc980fcf6531bf42bddebac
APALUI <83349874+RyanCrew@users.noreply.github.com>
2025-05-28 17:29:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener('install', event => {
             return cache.addAll([
                 './index.html',
                 './icon.png',
-                './icon-180.png'
+                './icon-180.png',
                 './favicon-32.png'
             ]);
         })
