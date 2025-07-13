aa91ce363f1487fcbe9452018d21a60d32689440
APALUI <83349874+RyanCrew@users.noreply.github.com>
2025-07-13 16:39:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('pram-selector-v1').then(cache => {
+        caches.open('pram-selector-v2').then(cache => {
             return cache.addAll([
                 './index.html',
                 './icon.png',
@@ -15,7 +15,7 @@ self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
-                cacheNames.filter(name => name !== 'pram-selector-v1').map(name => caches.delete(name))
+                cacheNames.filter(name => name !== 'pram-selector-v2').map(name => caches.delete(name))
             );
         })
     );
